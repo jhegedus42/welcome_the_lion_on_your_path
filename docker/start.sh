a334479db095297439a689ff6faf5b9e0a4c8b5d
Jozsef Hegedus <jhegedus@pm.me>
2019-10-01 23:27:06 +0300

    updating README.md, making it easy to get the image up and running

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,4 @@
-docker run  -p 5902:5902  -p 5901:5901  -p 5900:5900  -p 9000:8888  -p 8022:22  -e GRANT_SUDO=yes  -v /tmp:/tmp_of_host  --user joco  -it  --name welcome_the_lion_on_your_path_0009  jhegedus42/welcome_the_lion_on_your_path:0009  bash 
+version=`docker images |head -n 2 | tail -n 1| awk '{print $2}'`
+echo $version
+start_command="docker run  -p 5902:5902  -p 5901:5901  -p 5900:5900  -p 9000:8888  -p 8022:22  -e GRANT_SUDO=yes  -v /tmp:/tmp_of_host  --user joco  -it  --name welcome_the_lion_on_your_path_$version  jhegedus42/welcome_the_lion_on_your_path:$version  bash "
+echo $start_command
