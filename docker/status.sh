d7d1f5a33573bc289dfb53207629ef6c6f14eeae
Jozsef Hegedus <jhegedus@pm.me>
2019-11-27 08:19:55 +0100

    Status check files.

---
+++ docker/status.sh
@@ -0,0 +1,14 @@
+echo "---------------------------------------"
+echo "docker images:"
+echo
+/Users/joco/dev/docker/wellcome_the_lion_on_your_path/docker/print_docker_images.sh
+
+
+
+echo
+echo
+echo
+echo "---------------------------------------"
+echo "running docker containers:"
+echo
+docker ps -a
